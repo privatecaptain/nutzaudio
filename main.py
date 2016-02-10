5e7be783a226d4c2c99897ac2ca29de68987b0df
privatecaptain <happy1121.hb@gmail.com>
2016-02-10 10:17:50 -0500

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+import slate
+from Tkinter import *
+from tkFileDialog import *
+
+root = Tk()
+root.wm_title("PDF to CSV")
+
+w = Label(root, text="Please choose a .pdf file to convert.") 
+fileName = askopenfilename(parent=root)
+
+w.pack()
+root.mainloop()
+
+f = open(fileName,'rb')
+
+t1 = time.time()
+doc = slate.PDF(f)
+
+print time.time() - t1
+
+def get_info(page):
+	info = {}
+	page = remove_ship(page)
+	ship_to = page.find('Ship To')
+	bill_to = page.find('Bill To')
+	shipped_via = page.find('Shipped Via')
+	item_desc = page.find('Item Description')
+	name = page[bill_to:ship_to]
+	address = page[ship_to:item_desc]
+	return {
+		'name' : name.strip(),
+		'address' : address.strip()
+	}
+
+
+def remove_ship(page):
+	a = page.find('Shipped Via')
+	b = page.find('STANDARD')
+	return page[:a] + page[b+8:]
