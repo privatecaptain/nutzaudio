b47f8b71143b0ada83ddc725f05b78fa2f25433d
privatecaptain <happy1121.hb@gmail.com>
2016-02-11 20:57:48 -0500

    cleaner data

--- main.py
+++ main.py
@@ -10,17 +10,26 @@ f = open(fileName,'rb')
 t1 = time.time()
 doc = slate.PDF(f)
 
-print time.time() - t1
+print time.time() - t1, 'secs..'
 
 def get_info(page):
 	info = {}
 	page = remove_ship(page)
 	ship_to = page.find('Ship To')
 	bill_to = page.find('Bill To')
-	shipped_via = page.find('Shipped Via')
 	item_desc = page.find('Item Description')
-	name = page[bill_to:ship_to]
-	address = page[ship_to:item_desc]
+
+	if ship_to < bill_to:
+		name = page[bill_to:item_desc]
+		address = page[ship_to:bill_to]
+	else:
+
+		name = page[bill_to:ship_to]
+		address = page[ship_to:item_desc]
+	
+	# Clean strings
+	name = name.replace('Bill To','')
+	address = address.replace('Ship To','')
 	return {
 		'name' : name.strip(),
 		'address' : address.strip()
@@ -28,6 +37,15 @@ def get_info(page):
 
 
 def remove_ship(page):
-	a = page.find('Shipped Via')
-	b = page.find('STANDARD')
-	return page[:a] + page[b+8:]
+	page = page.replace('Shipped Via','')
+	page = page.replace('STANDARD','')
+	return page
+
+info = []
+
+for idx,e in enumerate(doc):
+	temp = get_info(e)
+	temp['id'] = idx
+	if temp['name'] != '' and temp['address'] != '':
+		info.append(temp)
+
